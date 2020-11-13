d5ac1cf421c162ad8955b5101f95dc18506f994b
mikhawa <michaeljpitz@gmail.com>
2020-11-13 08:39:26 +0100

    create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// DB's constants
+define("DB_HOST","localhost");
+define("DB_NAME","menu_recursif");
+define("DB_USER","root");
+define("DB_PWD","");
+define("DB_PORT",3306);
+define("DB_CHARSET","utf8");
+
